5f40e63b1616910bc6863269716fd167c63763e2
newstreetpunk <newstreetpunk@yandex.ru>
2019-04-28 15:23:24 +0500

    Добавлен блок видео

--- js/main.js
+++ js/main.js
@@ -7,4 +7,46 @@ $(function(){
     		$('.all-cases').removeClass("active");
   		};
 	});
+	$('.btn-play').magnificPopup({
+		disableOn: 700,
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: true,
+
+		fixedContentPos: false,
+		type: 'iframe',
+		iframe: {
+  markup: '<div class="mfp-iframe-scaler">'+
+            '<div class="mfp-close"></div>'+
+            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+          '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+
+  patterns: {
+    youtube: {
+      index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+
+      id: 'v=', // String that splits URL in a two parts, second part should be %id%
+      // Or null - full URL will be returned
+      // Or a function that should return %id%, for example:
+      // id: function(url) { return 'parsed id'; }
+
+      src: 'https://www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe.
+    },
+    vimeo: {
+      index: 'vimeo.com/',
+      id: '/',
+      src: '//player.vimeo.com/video/%id%?autoplay=1'
+    },
+    gmaps: {
+      index: '//maps.google.',
+      src: '%id%&output=embed'
+    }
+
+    // you may add here more sources
+
+  },
+
+  srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+}
+	});
 });
