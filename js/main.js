d55f000e19955f82782305d03a52ef5a3670241d
newstreetpunk <newstreetpunk@yandex.ru>
2019-04-29 08:49:56 +0500

    target

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 $(function(){
 
 	$(window).scroll(function() {
-  		if ($(this).scrollTop() > $(window).height()) {
-    		$('.all-cases').addClass("active");
-  		} else {
-    		$('.all-cases').removeClass("active");
-  		};
+		if ($(this).scrollTop() > $(window).height()) {
+			$('.all-cases').addClass("active");
+		} else {
+			$('.all-cases').removeClass("active");
+		};
 	});
+	
 	$('.btn-play').magnificPopup({
 		disableOn: 700,
 		mainClass: 'mfp-fade',
@@ -16,37 +17,26 @@ $(function(){
 		fixedContentPos: false,
 		type: 'iframe',
 		iframe: {
-  markup: '<div class="mfp-iframe-scaler">'+
-            '<div class="mfp-close"></div>'+
-            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-          '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
-
-  patterns: {
-    youtube: {
-      index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
-
-      id: 'v=', // String that splits URL in a two parts, second part should be %id%
-      // Or null - full URL will be returned
-      // Or a function that should return %id%, for example:
-      // id: function(url) { return 'parsed id'; }
-
-      src: 'https://www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe.
-    },
-    vimeo: {
-      index: 'vimeo.com/',
-      id: '/',
-      src: '//player.vimeo.com/video/%id%?autoplay=1'
-    },
-    gmaps: {
-      index: '//maps.google.',
-      src: '%id%&output=embed'
-    }
-
-    // you may add here more sources
-
-  },
-
-  srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
-}
-	});
+			markup: '<div class="mfp-iframe-scaler">'+
+			'<div class="mfp-close"></div>'+
+			'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+			'</div>',
+			patterns: {
+				youtube: {
+					index: 'youtube.com/', 
+					id: 'v=',
+					src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+				},
+				vimeo: {
+					index: 'vimeo.com/',
+					id: '/',
+					src: '//player.vimeo.com/video/%id%?autoplay=1'
+				},
+				gmaps: {
+					index: '//maps.google.',
+					src: '%id%&output=embed'
+				}
+			},
+			srcAction: 'iframe_src', 
+		});
 });
